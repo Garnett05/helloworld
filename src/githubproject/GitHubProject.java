26bfcb9b297593ceb81ae0b4e5fe7b76911d0ae8
Charlles <charlleslimamota10@hotmail.com>
2019-06-15 22:37:54 -0300

    Afternoon messange

--- src/githubproject/GitHubProject.java
+++ src/githubproject/GitHubProject.java
@@ -5,7 +5,7 @@ public class GitHubProject {
     public static void main(String[] args) {
                        
         System.out.println("Morning!");
-        
+        System.out.println("Afternoon!");        
     }
     
 }
